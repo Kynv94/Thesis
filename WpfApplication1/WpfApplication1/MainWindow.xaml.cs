94086644f8e1f47d787f0c40898a76257f2b1870
Kynv94 <12520291@gm.uit.edu.vn>
2016-10-17 23:15:40 +0700

    Init

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MenList.Items.Add(new Packet()
+            {
+                ID = 1,
+                Src = "192.168.1.2",
+                Des = "192.168.2.2",
+                Proc = "TCP",
+                Len = 135,
+                Info = "Server Hello"
+            });
+            MenList.Items.Add(new Packet()
+            {
+                ID = 2,
+                Src = "192.168.2.2",
+                Des = "192.168.1.2",
+                Proc = "TCP",
+                Len = 149,
+                Info = "Reply Server"
+            });
+        }
+
+    }
+
+    class Packet
+    {
+        public long ID { get; set; }
+        public string Src { get; set; }
+        public string Des { get; set; }
+        public string Proc { get; set; }
+        public long Len { get; set; }
+        public string Info { get; set; }
+
+    }
+}
+

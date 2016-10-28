81169c0d1efebee643881b07758bed954b8ad278
Kynv94 <12520291@gm.uit.edu.vn>
2016-10-28 10:33:24 +0700

    Interface

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,46 +1,114 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
 
 namespace WpfApplication1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
         public MainWindow()
         {
             InitializeComponent();
-            MenList.Items.Add(new Packet()
+            // Retrieve the device list from the local machine
+
+
+            if (allDevices.Count == 0)
             {
-                ID = 1,
-                Src = "192.168.1.2",
-                Des = "192.168.2.2",
-                Proc = "TCP",
-                Len = 135,
-                Info = "Server Hello"
-            });
-            MenList.Items.Add(new Packet()
+                string message = "No interfaces found! Make sure WinPcap is installed.";
+                string caption = "Warning";
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
             {
-                ID = 2,
-                Src = "192.168.2.2",
-                Des = "192.168.1.2",
-                Proc = "TCP",
-                Len = 149,
-                Info = "Reply Server"
-            });
+                LivePacketDevice device = allDevices[i];
+                string name_device = device.Name + " ";
+                if (device.Description != null)
+                    name_device += device.Description;
+                cbDevice.Items.Add(name_device);
+            }
         }
 
+        private void cbDevice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(e.AddedItems[0].GetType());
+            //PacketDevice selectedDevice = (e.AddedItems[0] as ComboBoxItem).Content as LivePacketDevice;
+            String text = e.AddedItems[0] as String;
+            int deviceIndex;
+            for (deviceIndex = 0; deviceIndex != allDevices.Count; ++deviceIndex)
+            {
+                LivePacketDevice device = allDevices[deviceIndex];
+                if (text == (device.Name + " " + device.Description))
+                    break;
+            }
+
+            PacketDevice selectedDevice = allDevices[deviceIndex];
+
+            // Open the device
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,                                  // portion of the packet to capture
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    1000))                                  // read timeout
+            {
+                // Check the link layer. We support only Ethernet for simplicity.
+                if (communicator.DataLink.Kind != DataLinkKind.Ethernet)
+                {
+                    string message = "This program works only on Ethernet networks.";
+                    string caption = "Warning";
+                    MessageBoxButton buttons = MessageBoxButton.OK;
+                    MessageBoxResult result;
+
+                    result = MessageBox.Show(message, caption, buttons);
+                    return;
+                }
+
+                // Compile the filter
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter("ip and udp"))
+                {
+                    // Set the filter
+                    communicator.SetFilter(filter);
+                }
+
+                //            Console.WriteLine("Listening on " + selectedDevice.Description + "...");
+
+                // start the capture
+                communicator.ReceivePackets(0, PacketHandler);
+            }
+        }
+
+        // Callback function invoked by libpcap for every incoming packet
+        private void PacketHandler(Packet packet)
+        {
+
+            // print timestamp and length of the packet
+            MenList.Items.Add(packet);
+            
+            //IpV4Datagram ip = packet.Ethernet.IpV4;
+            //UdpDatagram udp = ip.Udp;
+
+            //// print ip addresses and udp ports
+            //Console.WriteLine(ip.Source + ":" + udp.SourcePort + " -> " + ip.Destination + ":" + udp.DestinationPort);
+
+        }
+    
     }
 
-    class Packet
-    {
-        public long ID { get; set; }
-        public string Src { get; set; }
-        public string Des { get; set; }
-        public string Proc { get; set; }
-        public long Len { get; set; }
-        public string Info { get; set; }
-
-    }
+    
 }
 
---
+++ WpfApplication1/WpfApplication1/Packet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace WpfApplication1
+{
+    //class Packet
+    //{
+    //    public DateTime date { get; set; }
+    //    public string Proc { get; set; }
+    //    public string Src { get; set; }
+    //    public int PortSrc { get; set; }
+    //    public string Des { get; set; }
+    //    public int PortDes { get; set; }
+    //    public string Info { get; set; }
+
+    //}
+}
